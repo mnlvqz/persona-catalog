22efc930357e7fb8bea0bcb66d89a2e31c159ab8
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2023-12-18 15:55:42 -0600

    test

--- gps-test.py
+++ gps-test.py
@@ -8,3 +8,20 @@ pi = pigpio.pi()
 
 pi.set_mode(RX_PIN, pigpio.INPUT);
 pi.set_mode(TX_PIN, pigpio.OUTPUT);
+
+def read_bit(pin):
+    return pi.read(pin)
+
+def write_bit(pin, value):
+    pi.write(pin, value) 
+
+try:
+    while True:
+        data = read_bit(RX_PIN)
+        print(data)
+        time.sleep(0.01)
+
+        write_bit(TX_PIN, 1)
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    pi.stop()
